9c0b89d114c4d25aff634aa3bc4ff37d65c002f3
apurvc99 <apurv@venwiz.com.com>
2022-11-07 02:19:11 +0530

    refactor: 🛠️ added postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,15 @@
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/*.stories.@(ts|tsx|js|jsx|mdx)",
     "../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)",
+    "../src/**/*.stories.@(ts|tsx|js|jsx|mdx)",
   ],
-  // stories: [
-  //   "../stories/**/*.stories.mdx",
-  //   "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  //   "*.stories.@(js|jsx|ts|tsx|md|mdx)",
-  //   "../src/**/*.stories.@(js|jsx|ts|tsx|md|mdx)",
-  // ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
+  ],
   framework: "@storybook/react",
 };
